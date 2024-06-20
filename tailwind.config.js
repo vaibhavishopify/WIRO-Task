fbcebb031213f44c19c305670b7ed0bf4dc6c0bf
dimple0630 <154866777+dimple0630@users.noreply.github.com>
2024-06-20 14:26:46 +0530

    Initial commit of live Shopify theme

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{liquid,html,js}"],
+  theme: {
+    extend: {
+      color:{
+        blue: '#202BCB',
+        orange: '#ecc94b',
+      },
+    },
+    
+  },
+  plugins: [],
+};
+
+
